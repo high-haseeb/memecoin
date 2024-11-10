da68f6ae9a0e674a914478f468fc0750229cd117
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-11-10 06:01:58 +0500

    [hack] moved gallery to / route

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Gallery from "./pages/VideoGallery";
 
     const router = createBrowserRouter(
         [
-            {path: "/", element: <Home/>},
+            {path: "/", element: <Gallery />},
             {path: "/gallery", element: <Gallery/>},
         ]
     )
